307b34ee547702915ff88f6b6de4b2500d23e39a
Erfun B. Sarmadi <erfanbs1380@gmail.com>
2025-09-25 23:47:17 +0330

    Update script.py

--- script.py
+++ script.py
@@ -10,11 +10,14 @@ def get_service_from_files():
     creds = Credentials.from_authorized_user_file('token.json', SCOPES)
     return build('gmail', 'v1', credentials=creds)
 
-def create_message(sender, to, subject, text, thread_id=None):
+def create_message(sender, to, subject, text, thread_id=None, reply_to_id=None):
     message = MIMEText(text)
     message['to'] = to
     message['from'] = sender
     message['subject'] = subject
+    if reply_to_id:
+        message['In-Reply-To'] = reply_to_id
+        message['References'] = reply_to_id
     raw = base64.urlsafe_b64encode(message.as_bytes()).decode()
     body = {'raw': raw}
     if thread_id:
@@ -33,5 +36,5 @@ if __name__ == "__main__":
     sent = send_message(service, "me", msg)
 
     # Example: follow-up on same thread
-    reply = create_message("me", "erfanbs1380@gmail.com", "Re: Daily Update", "This is a reminder!", sent["threadId"])
+    reply = create_message("me", "erfanbs1380@gmail.com", "Re: Daily Update", "This is a reminder!", thread_id = sent["threadId"])
     send_message(service, "me", reply)
