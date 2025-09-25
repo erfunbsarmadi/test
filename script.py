adbe02bb43be7a32c91589d5b2d3f3601cc5f6b2
Erfun B. Sarmadi <erfanbs1380@gmail.com>
2025-09-25 21:11:22 +0330

    Update script.py

--- script.py
+++ script.py
@@ -3,26 +3,31 @@ import smtplib
 from email.message import EmailMessage
 import uuid
 
-email_address = os.getenv("GMAIL_ADDRESS")
-email_password = os.getenv("GMAIL_APP_PASSWORD")
-print(email_address)
-print(email_password)
-
-if not email_address or not email_password:
-    raise ValueError("Missing Gmail credentials. Did you set GitHub Secrets?")
-
-msg = EmailMessage()
-msg["Subject"] = "Test Email"
-msg["From"] = email_address
-msg["To"] = "erfanbs1380@gmail.com"
-msg["Message-ID"] = f"<{uuid.uuid4()}@gmail.com>"
-msg.set_content("Hello from Python + Gmail!")
-
-with smtplib.SMTP("smtp.gmail.com", 587) as smtp:
-    smtp.starttls()
-    smtp.login(email_address, email_password)
-    smtp.send_message(msg)
+def sendMail(to,subject,content,**kwargs):
+    email_address = os.getenv("GMAIL_ADDRESS")
+    email_password = os.getenv("GMAIL_APP_PASSWORD")
+    print(email_address)
+    print(email_password)
+    
+    if not email_address or not email_password:
+        raise ValueError("Missing Gmail credentials. Did you set GitHub Secrets?")
+    
+    msg = EmailMessage()
+    msg["Subject"] = subject
+    msg["From"] = email_address
+    msg["To"] = to
+    if 'errormessage' in kwargs:
+        msg["Message-ID"] = f"<{uuid.uuid4()}@gmail.com>"
+    else:
+        msg["Message-ID"] = kwargs["Message-ID"]
+    msg.set_content(content)
+    
+    with smtplib.SMTP("smtp.gmail.com", 587) as smtp:
+        smtp.starttls()
+        smtp.login(email_address, email_password)
+        smtp.send_message(msg)
 
 if __name__ == __main__:
+    sendMail("erfanbs1380@gmail.com", "Test Email", "Hello from Python + Gmail!")
     print("Email sent successfully!")
     print("Message-ID:", msg["Message-ID"])
