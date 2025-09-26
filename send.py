3067969e79b81e26bbaacdd33e2675a8da15c661
Erfun B. Sarmadi <erfanbs1380@gmail.com>
2025-09-26 04:18:19 +0330

    Update send.py

--- send.py
+++ send.py
@@ -29,6 +29,7 @@ def send_message(service, user_id, body):
 if __name__ == "__main__":
     service = get_service_from_files()
     # Example: send initial email and save threadId
-    sent = send_message(service, 'me', create_message('me', 'erfanbs1380@gmail.com', 'Test API', 'Hello from API!'))
+    recipient = email_address = os.getenv("RECIPIENT_EMAIL_ADDRESS")
+    sent = send_message(service, 'me', create_message('me', recipient, 'Test API', 'Hello from API!'))
     print("Official Gmail message id:", sent.get('id'))
     print("threadId:", sent.get('threadId'))
