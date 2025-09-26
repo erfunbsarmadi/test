4739ee34ac46f425bc7a30b87d67dff961d834ba
Erfun B. Sarmadi <erfanbs1380@gmail.com>
2025-09-26 03:49:24 +0330

    Update script.py

--- script.py
+++ script.py
@@ -75,7 +75,7 @@ def send_message(service, body):
 if __name__ == "__main__":
     service = get_service()
     recipient = "erfanbs1380@gmail.com"
-    subject = "Research Assistantship Availability Inquiry"
+    subject = "Application for Research Assistantship in Surena Project"
 
     # Step 1: Locate the conversation across *all* Sent messages
     convo = find_conversation(service, recipient, subject)
