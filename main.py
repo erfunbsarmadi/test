c094c5cf76a0203f24065e2f305289f37a40bfa1
Erfun B. Sarmadi <erfanbs1380@gmail.com>
2025-09-27 13:46:04 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from gdrive_helper import get_drive_service
 from sheet_helper import read_sheet, write_sheet
 from outlook_mailer import get_token, send_email
+import pandas as pd
 
 # --- Setup ---
 drive_service = get_drive_service()
@@ -8,5 +9,5 @@ creds = drive_service._http.credentials  # reuse creds
 sheet_id = "1j3TazOWluMGJZRk9TweKadKpIaE00wZ7coSyjsjcMIQ"
 
 # --- Get recipients ---
-emails = read_sheet(sheet_id, "Sheet1!1:1000", creds)
-print(emails)
+df = read_sheet(sheet_id, "Sheet1!1:1000", creds)
+print(df)
