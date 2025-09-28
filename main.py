cb58c72a4ca154f88d920445705ac6c11da7011d
Erfun B. Sarmadi <erfanbs1380@gmail.com>
2025-09-28 13:13:10 +0330

    Update main.py

--- main.py
+++ main.py
@@ -7,22 +7,27 @@ import pandas as pd
 sheet_id = "1j3TazOWluMGJZRk9TweKadKpIaE00wZ7coSyjsjcMIQ"
 creds_file = "credentials.json"
 
-# --- Get recipients ---
 df = read_sheet(sheet_id, "Sheet1!1:1000", creds_file)
-print(df['Email'][0])
-df.loc[0, 'Email'] = 'erfanbs1380@gmail.com'
-print(df['Email'][0])
 
-write_sheet(sheet_id, 'Sheet1!A1', df, creds_file)
-
-download_file('1zvBoRn_5hlhoiSuhptqtD6CdrgxlAryg', 'cv.pdf')
 
 token = get_token()
 
-recipients = ["erfanbs1380@gmail.com"]
-subject = "Report attached"
-body = "<p>Hello,<br>Here is your PDF report.</p>"
+#prepare email
+
+
+
+#check email
+#suggest subject
+#check subject
+
+download_file('1zvBoRn_5hlhoiSuhptqtD6CdrgxlAryg', 'cv.pdf')
+
+
+
+
 
 send_email(token, recipients, subject, body, attachments = ['cv.pdf'])
 
+
+write_sheet(sheet_id, 'Sheet1!A1', df, creds_file)
 cleanup_file('cv.pdf')
