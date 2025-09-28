e68d4d2aa1098f076f56ee7c6a7fec13b65e936d
Erfun B. Sarmadi <erfanbs1380@gmail.com>
2025-09-28 14:48:47 +0330

    Update gemini_helper.py

--- gemini_helper.py
+++ gemini_helper.py
@@ -6,7 +6,7 @@ def compose_email(lastName, abstract):
     client = genai.Client()
     prompt = os.getenv("COMPOSITION_PROMPT")
     body = os.getenv("EMAIL_BODY")
-    contact = 'Dear Professor' + lastName + ','
+    contact = 'Dear Professor ' + lastName + ','
     contents = prompt + '\n' + contact + '\n' + body + '\n' + abstract
     
     response = client.models.generate_content(
