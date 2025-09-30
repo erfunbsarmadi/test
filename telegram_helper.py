b5b9fe8ccc27ee5e90299363527693fe62195fbb
Erfun B. Sarmadi <erfanbs1380@gmail.com>
2025-09-30 12:01:07 +0330

    Update telegram_helper.py

--- telegram_helper.py
+++ telegram_helper.py
@@ -18,7 +18,7 @@ def send_message(token, chatID, text, parse_mode = 'HTML'):
     response = requests.get(url)
     
     # Raise an error if something went wrong
-    response.raise_for_status()
+    #response.raise_for_status()
     
     # Parse JSON response
     updates = response.json()
@@ -36,4 +36,5 @@ if __name__ == "__main__":
     # Pretty print JSON
     print(json.dumps(updates, indent=4, ensure_ascii=False))
 
-    send_message(token, chatID, text)
+    updates = send_message(token, chatID, text)
+    print(json.dumps(updates, indent=4, ensure_ascii=False))
