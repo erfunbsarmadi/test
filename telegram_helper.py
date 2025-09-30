7d603c0056ffbe003947c3e4c6364d4ab70804d4
Erfun B. Sarmadi <erfanbs1380@gmail.com>
2025-09-30 11:11:23 +0330

    Create telegram_helper.py

---
+++ telegram_helper.py
@@ -0,0 +1,24 @@
+import requests
+import json
+import os
+
+def get_updates(token):
+    url = f"https://api.telegram.org/bot{token}/getUpdates"
+    response = requests.get(url)
+    
+    # Raise an error if something went wrong
+    response.raise_for_status()
+    
+    # Parse JSON response
+    updates = response.json()
+    return updates
+
+
+if __name__ == "__main__":
+    # Replace with your bot token
+    BOT_TOKEN = os.getenv()
+    
+    updates = get_updates(BOT_TOKEN)
+    
+    # Pretty print JSON
+    print(json.dumps(updates, indent=4, ensure_ascii=False))
