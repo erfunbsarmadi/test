00ea7dd78d7053458ad29f1a4dfbf962f05fd605
Erfun B. Sarmadi <erfanbs1380@gmail.com>
2025-09-30 11:42:17 +0330

    Update telegram_helper.py

--- telegram_helper.py
+++ telegram_helper.py
@@ -14,7 +14,7 @@ def get_updates(token, updateID):
     return updates
 
 def send_message(token, chatID, text, parse_mode = 'HTML'):
-    url = f"https://api.telegram.org/bot{token}/sendMessage?chat_id={chatID}&text={text},parse_mode={parse_mode}"
+    url = f"https://api.telegram.org/bot{token}/sendMessage?chat_id={chatID}&text={text}&parse_mode={parse_mode}"
     response = requests.get(url)
     
     # Raise an error if something went wrong
