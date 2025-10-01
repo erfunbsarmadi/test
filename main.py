1d5700bbaf1c2ca82068ef426a4be625b5b3204a
Erfun B. Sarmadi <erfanbs1380@gmail.com>
2025-10-02 00:41:34 +0330

    Update main.py

--- main.py
+++ main.py
@@ -18,91 +18,23 @@ df['Replied'] = df['Replied'].astype(int)
 
 #prepare email
 i = 0
-if df['Last Email Sent'][i] == '':
-    #clarity_check_result = 'Negative'
-    #while clarity_check_result != 'Positive':
-    #    lastName = df['Professor Name'][i]
-    #    abstract = df['Abstract'][i]
-    #    text = compose_email(lastName, abstract)
-    #    clarity_check_result = clarity_check(text)
-    #    print(clarity_check_result)
-    #df['Email Body'][i] = text
-    
-    #clarity_check_result = 'Negative'
-    #while clarity_check_result != 'Positive':
-    #    emailBody = df['Email Body'][i]
-    #    text = suggest_subject(emailBody)
-    #    clarity_check_result = clarity_check(text)
-    #    print(clarity_check_result)
-    #df['Subject'][i] = text
-    
-    #token = get_token()
-    #recipient = df['Email'][i]
-    #subject = df['Subject'][i]
-    #body = df['Email Body'][i]
- #   if send_email(token, recipient, subject, body, attachments = ['CV', 'BSc Transcripts', 'MSc Transcripts']):
+if df['Last Email Sent'][i] == '' and datetime.datetime.now().weekday() < 6:
     lastName = df['Professor Name'][i]
     abstract = df['Abstract'][i]
     body = compose_email(lastName, abstract)
     subject = suggest_subject(emailBody)
-    text = f'''
-    index = {i}\n
-    Subject : {subject}\n
-    Email Body :\n
-    {body}'''
-    send_message(text)
-    
-    if True:
-        date = datetime.datetime.now()
-        df['Last Email Sent'][i] = date.strftime("%a %d/%b/%Y")
-        df['Email Body'][i] = 'First Email:\n' + df['Email Body'][i]
-        
-        delta = randint(7,14)
-        date = date + datetime.timedelta(days=delta)
 
-        df['Planned Reminder Date'][i] = date.strftime("%a %d/%b/%Y")
-
-elif datetime.datetime.now().strftime("%a %d/%b/%Y") == df['Planned Reminder Date'][i] and df['Reminders Sent'][i] < 5 and df['Replied'][i] == 0:
-    clarity_check_result = 'Negative'
-    while clarity_check_result != 'Positive':
-        emailBody = df['Email Body'][i]
-        text = compose_reminder(emailBody)
-        clarity_check_result = clarity_check(text)
-        print(clarity_check_result)
-
-    token = get_token()
-    recipient = df['Email'][i]
+elif datetime.datetime.now().strftime("%a %d/%b/%Y") == df['Planned Reminder Date'][i] and df['Reminders Sent'][i] < 5 and df['Replied'][i] == 0:    
+    body = compose_reminder(emailBody)
     subject = 'Reminder: ' + df['Subject'][i]
-    body = text
-  #  if send_email(token, recipient, subject, body, attachments = ['CV', 'BSc Transcripts', 'MSc Transcripts']):
-    if True:
-        date = datetime.datetime.now()
-        df['Last Email Sent'][i] = date.strftime("%a %d/%b/%Y")
-        df['Reminders Sent'][i] = df['Reminders Sent'][i] + 1
-        df['Email Body'][i] = df['Email Body'][i] + '\n\nReminder ' + str(df['Reminders Sent'][i]) + ':\n' + text
-        
-        while True:
-            delta = randint(7,14)
-            date = date + datetime.timedelta(days=delta)
-            if date.weekday() < 6:
-                break
-            else:
-                delta = -1 * delta
-                date = date + datetime.timedelta(days=delta)
 
-        df['Planned Reminder Date'][i] = date.strftime("%a %d/%b/%Y")
+text = f'''
+index = {i}\n
+Subject : {subject}\n
+Email Body :\n
+{body}'''
+send_message(text)
 
-#check email
-#suggest subject
-#check subject
-
-#download_file('1zvBoRn_5hlhoiSuhptqtD6CdrgxlAryg', 'cv.pdf')
-
-
-
-
-
-#send_email(token, recipients, subject, body, attachments = ['cv.pdf'])
 
 
 write_sheet(sheet_id, 'Sheet1!A1', df, creds_file)
